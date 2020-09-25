01f7867e77d8475300958584503ad48b6092b45c
Daniel Gonçalves da Silva <danielgoncalvesti@gmail.com>
2020-09-24 21:13:29 -0300

    first commit

---
+++ download.php
@@ -0,0 +1,50 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.require_once '../../config.php';
+
+require_once '../../../../config.php';
+global $USER, $DB, $CFG;
+
+require_login();
+
+$dataformat = optional_param('dataformat', '', PARAM_ALPHA);
+$courseid = optional_param('courseid', '', PARAM_INT);
+$quizid = optional_param('quizid', '', PARAM_INT);
+
+$sql = "SELECT v.id, v.username, v.facedetectionscore, v.euclidean_distance, v.timecreated FROM {fvquiz_validation} v WHERE courseid = 2 AND quizid = 1";
+$validations = $DB->get_records_sql($sql);
+
+
+$obj = new ArrayObject( $validations);
+$it = $obj->getIterator();
+
+$columns = array(
+    'username' => get_string('username', 'quiz_faceverificationreport'),
+    'facedetectionscore' => get_string('facedetectionscore', 'quiz_faceverificationreport'),
+    'euclidean_distance' => get_string('euclidean_distance', 'quiz_faceverificationreport'),
+    'timecreated' => get_string('timecreated', 'quiz_faceverificationreport'),
+);
+
+\core\dataformat::download_data('graderdata', $dataformat, $columns, $it, function($record) {
+
+    $record->timecreated = date('d-M-Y H:m',  $record->timecreated);
+    unset($record->id);
+    // unset($record->tmodified);
+      // Process the data in some way.
+      // You can add and remove columns as needed
+      // as long as the resulting data matches the $column metadata.
+      return $record;
+});
---
+++ report.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
+
+
+class quiz_quiz_faceverificationreport_report extends quiz_default_report {
+
+    public function display($quiz, $cm, $course) {
+        global $OUTPUT, $DB;
+
+        $results = new stdClass();
+        $this->context = context_module::instance($cm->id);
+
+        $courseid = $quiz->course;
+        $quizid =  $quiz->id;
+
+        $sql = "SELECT v.id, v.username, v.facedetectionscore, v.euclidean_distance, v.timecreated FROM {fvquiz_validation} v WHERE courseid = $courseid AND quizid = $quizid";
+        $validations = $DB->get_records_sql($sql);
+
+
+        foreach ($validations as $key => $value) {
+
+            $validations[$key]->timecreated = date('d-M-Y H:m',$validations[$key]->timecreated);
+        }
+
+        // Start output.
+        $this->print_header_and_tabs($cm, $course, $quiz, 'quiz_faceverificationreport');
+
+        $results->data = array_values($validations);
+
+        echo $OUTPUT->render_from_template('quiz_faceverificationreport/searchresults', $results);
+        echo $OUTPUT->download_dataformat_selector('Download', 'report/faceverificationquiz/download.php', 'dataformat', array('couseid' => $courseid, 'quizid' => $quizid));
+
+    }
+}
